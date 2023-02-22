8e2d5d9d846ab003758e7154a799ef7f087f2aeb
Kamdev Kumar <87974605+kamdev66@users.noreply.github.com>
2023-02-22 10:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function calculateMinCost() {
   //your code here
-  
+   const heap = new MinHeap(ropes);
+  let cost = 0;
+
+  while (heap.size() > 1) {
+    const rope1 = heap.extractMin();
+    const rope2 = heap.extractMin();
+    const newRope = rope1 + rope2;
+    cost += newRope;
+    heap.insert(newRope);
+  }
+
+  return cost;
   
   
 }  
